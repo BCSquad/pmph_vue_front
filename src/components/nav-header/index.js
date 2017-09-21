562fbfc9409a87c969c5aace9930e3fc38e68912
gugia <gugia@ANantes-651-1-50-1.w2-0.abo.wanadoo.fr>
2017-09-21 18:09:54 +0800

    First commit.

---
+++ src/components/nav-header/index.js
@@ -0,0 +1,8 @@
+require('./index.scss');
+
+var header = (function() {
+    console.log('this is nav')
+	Ipmph.UserModel.init();
+})();
+
+module.exports = header;
---
+++ src/pages/index/js/index.js
@@ -0,0 +1,79 @@
+require('components/nav-header/index');
+require('components/footer/index');
+
+require('../sass/layout.scss');
+
+
+// init();
+
+// function init() {
+	// const a = 1;
+	// console.log(a);
+	//
+	// var b = ['1','2d','4f']
+	//
+	// var c = b.map(function (iterm) {
+		// return parseInt(iterm);
+    // });
+	// console.log(c);
+	//
+    // let d  = function (t) {
+    //     var n = t && t.__esModule ? function () {
+    //         return t.default
+    //     } : function () {
+    //         return t
+    //     };
+    //     return n
+    // }.bind(this)
+
+    // let promise = new Promise(function(resolve,reject){
+		// setTimeout(function () {
+    //         resolve('success')
+    //     },3000)
+    // })
+    //
+    // promise.then(function (val) {
+		// console.log(val);
+    // })
+// }
+
+// var promise = new Promise(function(resolve){
+// 	resolve(42);
+// })
+//
+// promise.then(function(value){
+// 	console.log(value);
+// 	return 444
+// }).then(function (value) {
+// 	console.log(value,' this is two')
+// }).catch(function (e) {
+// 	console.log(e);
+// });
+
+// class MyError extends Error{
+// 	// 继承了Error类的对象
+// }
+
+
+$(function () {
+	Ipmph.UserModel.on(Ipmph.UserModel.eventType.LOGIN_SUCCESS,function (data) {
+		console.log('接收到登录成功事件');
+		Ipmph.message.success('接收到登录成功事件');
+		// console.log(data)
+	})
+
+	var options = {
+		totalPages: 100,
+		onPageChange: function (num, type) {
+		}
+	}
+
+	$('#example').jqPaginator(options);
+
+	$('.btn').on('click',function(){
+		var type = $(this).attr('data-type');
+
+        Ipmph.message[type]('这是一段提示文字信息');
+	})
+
+});
