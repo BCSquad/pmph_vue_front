0ea4253ea1cf4e3f19870e8b679712f8d97a8671
huangwei9527 <1240413812@qq.com>
2017-09-22 12:58:40 +0800

    开发新页面登录页面，增加bootstrap的配置文件

--- src/pages/login/js/index.js
+++ src/pages/login/js/index.js
@@ -2,3 +2,27 @@ require('components/nav-header/index');
 require('components/footer/index');
 
 require('../sass/layout.scss');
+
+//引入canvas背景计算方法
+import {CanvasBackground} from '@/libs/canvas_bg.js';
+
+$(function () {
+
+	/**
+	 * 初始化canvas背景
+	 */
+	var bg=new CanvasBackground({
+		canvasContainerID:"canvas",
+		circleColor:"rgba(49,210,142,0.8)",
+		lineColor:"rgba(49,210,142,1)",
+		canvasOpacity:0.2
+	});
+
+	/**
+	 * 登录按钮点击事件
+	 */
+	$("#loginBtn").on('click',function () {
+		Ipmph.message.info('我还没做登录功能！');
+	})
+
+})
